6f91f4d9d6d38b336d33e0425e1994daa5bc25e2
Ranee Bracker <rbracker@asu.edu>
2023-12-18 15:29:21 -0700

    finished up models folder

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -3,4 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db; -- did not exist rb
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db; --created DB rb
-
--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    }, // added rb
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, // added rb
   },
   {
     sequelize,
