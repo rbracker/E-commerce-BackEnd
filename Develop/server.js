edeeaaf54d4cbf64d5540c4fdabed50eedcd6cd3
Ranee Bracker <rbracker@asu.edu>
2023-12-19 21:38:07 -0700

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection'); // added rb
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,9 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => { //added rb
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
